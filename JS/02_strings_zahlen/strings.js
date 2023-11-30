f7732a70149ce4d046fa95f3320ab8048f1d236f
cihan3831 <cihan.sahin@student.htldornbirn.at>
2023-11-30 09:07:59 +0100

    02_js done

---
+++ JS/02_strings_zahlen/strings.js
@@ -0,0 +1,29 @@
+// Berechne das Ergebnis von 17.4 / 3.9. Gib folgenden Satz aus: 
+
+// The result of 17.4 divided by 3.9 is 4.461538461538462 
+
+// Lege eine Variable firstname und eine Varialbe lastname an. Weise den Variablen Deinen Namen zu. Gib folgendes aus: 
+
+// "Ich heiße VORNAME NACHNAME" 
+
+let a = 17.4;
+let b = 3.9;
+
+let result = a - b;
+console.log(result);
+
+console.log("Das Ergebnis von " + a + " und " + b + " ist " + result);
+
+let result2 = a / b;
+console.log(result2);
+
+console.log("Das Ergebnis von " + a + " dividiert durch " + b + " ist " + result2);
+
+
+let firstname = "Cihan";
+console.log(firstname);
+
+let lastname = "Sahin";
+console.log(lastname);
+
+console.log("Ich heiße " + firstname + " " + lastname);
