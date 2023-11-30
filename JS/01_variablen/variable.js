1784ba5f12b3a9e5912e7861f08ff27930949b76
cihan3831 <cihan.sahin@student.htldornbirn.at>
2023-11-30 08:30:12 +0100

    01_js done

--- JS/01_variablen/variable.js
+++ JS/01_variablen/variable.js
@@ -12,23 +12,51 @@ let c = 10;
 // Berechne folgendes und gib das Eregebnis über console.log aus 
 
 // a + b + c 
+let result = a + b + c;
+    console.log(result);
 
 // (a + b) / c 
+let result2 = (a + b) / c;
+    console.log(result2);
 
 // c -a + b 
+let result3 = c - a + b;
+    console.log(result3);
 
 // a + b + c * 3 
+let result4 = a + b + c * 3;
+    console.log(result4);
 
-// (a + b + c)/4 
+// (a + b + c)/4
+let result5 = (a + b + c)/4;
+    console.log(result5);
 
  
 
 // Ändere die Variablen auf folgende Werte 
 
 // a = 3 
+a = 3;
 
 // b = a + 3 
+b = a + 3;
 
 // c = c -3 
+c = c - 3;
 
 // gib die neuen Werte der Variablen aus 
+
+let result6 = a + b + c;
+    console.log(result6);
+
+let result7 = (a + b) / c;
+    console.log(result7);
+
+let result8 = c - a + b;
+    console.log(result8);
+
+let result9 = a + b + c * 3;
+    console.log(result9);
+
+let result10 = (a + b + c)/4;
+    console.log(result10);
