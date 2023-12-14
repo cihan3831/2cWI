bf1a1005932e6bb3ca5c6f18e5239a5f3881e3bf
cihan3831 <cihan.sahin@student.htldornbirn.at>
2023-12-14 09:08:00 +0100

    04_schleifen done

--- JS/04_schleifen/schleifen.js
+++ JS/04_schleifen/schleifen.js
@@ -1,17 +1,45 @@
 // Zähle von 7 bis 45 
-for (let count= 7; count < 45; count++){
+for (let count = 7; count <= 45; count ++){
     console.log(count);
 }
+
 console.log("Fertig");
 
 // Zähle von 9 bis 99 
+for (let count = 9; count <= 99; count ++){
+    console.log(count);
+}
+
+console.log("Fertig");
 
 // Zähle rückwärts von 10 bis 0 
+for (let count = 10; count >= 0; count --){
+    console.log(count);
+}
+
+console.log("Fertig");
 
 // Zähle rückwärts von 1234 bis 1207 
+for (let count = 1234; count >= 1207; count --){
+    console.log(count);
+}
 
- 
+console.log("Fertig");
 
 // Gib Deinen Namen genau 8 mal aus 
+for (let count = 0; count < 8; count ++){
+    console.log("Cihan");
+}
+
+console.log("Fertig");
 
 // Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0 
+for (let count = 0; count <= 10; count ++){
+    console.log(count);
+}
+
+console.log("Cihan");
+
+for (let count = 10; count >= 0; count --){
+    console.log(count);
+}
