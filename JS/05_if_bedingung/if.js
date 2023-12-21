b62b1dfb3c222cc808c168ab516458e8c2458adb
cihan3831 <cihan.sahin@student.htldornbirn.at>
2023-12-21 09:00:33 +0100

    05_if_bedingung finished

--- JS/05_if_bedingung/if.js
+++ JS/05_if_bedingung/if.js
@@ -1,23 +1,40 @@
 // erstelle eine Variable number 
 
- 
+ let number = 10;
 
 // Aufgabe 1 
 
 // Wenn die Zahl kleiner ist als 20  gib aus "Mini" 
+if (number < 20){
+    console.log("Mini");
+}
 
 // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium" 
+else if (number < 50){
+    console.log("Medium");
+}
 
 // Wenn die Zahl größer als 50 ist gib aus "Large" 
+if (number>50) {
+    console.log("Large");
+}
 
- 
-
- 
 
 // Aufgabe 2 
 
+let number2 = 54;
+
 // Wenn die Zahl kleiner gleich 7 ist gib aus "Mini" 
+if (number2<=7){
+    console.log("Mini");
+}
 
 // Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium" 
+else if (number2<90){
+    console.log("Medium");
+}
 
-// Wenn die Zahl größer als 90 ist gib aus "Large" 
+// Wenn die Zahl größer als 90 ist gib aus "Large"
+if (number2>90) {
+    console.log("Large");
+}
