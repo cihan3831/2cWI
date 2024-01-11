1993b2286abff6765744dc8594db4d3ca5590113
cihan3831 <cihan.sahin@student.htldornbirn.at>
2024-01-11 09:56:21 +0100

    end of lesson

---
+++ JS/07_logischeOperatoren/logisch.js
@@ -0,0 +1,21 @@
+// Erstelle zwei Zufallszahl zwischen 0 und 100 
+
+// Speichere diese jeweils in einer Variable 
+
+ 
+
+// Wenn die erste Zahl kleiner ist als die zweite UND die erste Zahl kleiner ist als 50 
+
+// dann gib aus "Zahl 1 ist kleiner als Zahl 2 und Mini" 
+
+ 
+
+// Wenn die erste Zahl kleiner ist als 30 oder die zweite Zahl kleiner ist als 30 
+
+// dann gib aus "Eine der beiden ist kleiner als 30" 
+
+ 
+
+// Wenn die erste Zahl kleiner ist als 50 UND die zweite Zahl ungleich 50 ist 
+
+// dann gib aus "Erste Zahl klein, zweite kein 50iger" 
