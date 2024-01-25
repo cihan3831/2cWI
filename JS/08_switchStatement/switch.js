e3ec84d8b180ea963ab34493b709b3a1658ec4cc
cihan3831 <cihan.sahin@student.htldornbirn.at>
2024-01-25 09:05:21 +0100

    08_switchStatement finished

--- JS/08_switchStatement/switch.js
+++ JS/08_switchStatement/switch.js
@@ -1,11 +1,30 @@
  // Erstelle eine Zufallszahl zwischen 5 und 10 
-
+let randomNumber = Math.random() * 6;
+let random = Math.floor(randomNumber) + 5;
+console.log(random);
  
-
+let a = random;
 // Wenn der Wert 10 ist gib aus Ten 
+if (a==10) {
+    console.log("Ten");
+}
 
 // Wenn der Wert 9 ist gib aus Nine 
+if (a==9) {
+    console.log("Nine");
+}
 
 // Wenn der Wert 8 ist gib aus Eight 
+if (a==8) {
+    console.log("Eight");
+}
 
-// etc. 
+// Wenn der Wert 7 ist gib aus Eight
+if (a==7) {
+    console.log("Seven");
+}
+
+// Wenn der Wert 6 ist gib aus Eight 
+if (a==6) {
+    console.log("Six");
+}
