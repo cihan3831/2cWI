d2dd7632090256530acb31502bb750d48822420b
cihan3831 <cihan.sahin@student.htldornbirn.at>
2024-01-25 09:28:45 +0100

    09_zeichen angefangen

---
+++ JS/09_zeichen/zeichen.js
@@ -0,0 +1,8 @@
+//Erstelle mit Hilfe einer Schleife und Modulo das folgende Ausgabe liefert (Rechteckbeispiel adaptieren):  
+
+//X X X X
+//O O O O  
+//X X X X  
+//O O O O  
+
+ //2. Versuche in einer for - Schleife (1-100) alle geraden Zahlen zu addieren (zwei Lösungswege ⇒ Zähler dahingehend verändern das er nur gerade Zahlen zählt und mit Modulo arbeiten.)
