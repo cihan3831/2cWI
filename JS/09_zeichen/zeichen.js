164599e5fa1b998f5d46426b2138dbae7563a5e2
cihan3831 <cihan.sahin@student.htldornbirn.at>
2024-02-15 09:14:16 +0100

    09_zeichen done

--- JS/09_zeichen/zeichen.js
+++ JS/09_zeichen/zeichen.js
@@ -5,4 +5,22 @@
 //X X X X  
 //O O O O  
 
+for (let index = 0; index <= 3; index++) {
+    if (index % 2 == 0) {
+        console.log("XXXX");
+    }
+    else {
+        console.log("OOOO");
+    }
+}
+
  //2. Versuche in einer for - Schleife (1-100) alle geraden Zahlen zu addieren (zwei Lösungswege ⇒ Zähler dahingehend verändern das er nur gerade Zahlen zählt und mit Modulo arbeiten.)
+let counter = 0;
+for (let index = 1; index < 100; index++) {
+    if (index % 2 == 0) {
+        counter = counter + index;
+        console.log(index);
+    }    
+}
+console.log(counter);
+
