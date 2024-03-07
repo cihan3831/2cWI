58000b9b4952642279f442730e2b451611a59149
cihan3831 <cihan.sahin@student.htldornbirn.at>
2024-03-07 09:24:47 +0100

    09_zeichen1_done

--- JS/09_zeichen1/zeichen1.js
+++ JS/09_zeichen1/zeichen1.js
@@ -5,18 +5,19 @@ If the number is even: Add it to the output
 */ 
 
 let output = 0; 
-
 const data = "4 0 1 2 3 4 5 6 8 1"; 
-
 var inputs = data.split(" "); 
-
 for (let i = 0; i < inputs.length; i++) { 
-
   const number = parseInt(inputs[i]); 
 
-} 
+if (number % 2 === 0) {
+  output = output + number
+} else {
+  output = output * number;   
+}
+
+console.log(output);
+}
 
 // Write an answer using console.log() 
 // To debug: console.error('Debug messages...'); 
-
-console.log("output:" + output); 
---
+++ JS/10_funktionen/funktionen.js
@@ -0,0 +1,6 @@
+printmenu("Jan", 17, "Lustenau");
+printmenu("Cihan", 38, "Hohenems");
+
+function add(a,b); {
+    return a + b -
+}
