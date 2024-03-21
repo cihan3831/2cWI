1adecf84f1184ccf79b0cbbf5b7b3fc98f361e26
cihan3831 <cihan.sahin@student.htldornbirn.at>
2024-03-21 09:29:06 +0100

    11 angefangen

---
+++ JS/11_arrays/arrays.js
@@ -0,0 +1,39 @@
+//Aufgabe 1 
+
+ 
+
+//Erstelle ein Array mit folgenden Werten: 4,1,2,3 
+
+//Füge die Werte 17 und 199 dazu 
+
+ 
+
+//Gib alle Werte in einer Schleife aus! 
+
+//Zähle alle Werte zusammen und gib das Ergebnis aus 
+
+//Berechne den Mittelwert der Zahlen und gib das Ergebnis aus
+
+ 
+
+ 
+
+//Aufgabe 2 
+
+ 
+
+//Erstelle ein Array mit folgenden Strings: 
+
+//"Susi", "Paula", "Hans" 
+
+ 
+
+//Gib folgenden Satz aus: 
+
+//"Meine Freunde sind Susi, Paula und Hans 
+
+ 
+
+//Füge "Sepp" dazu. Nun soll die ausgabe folgendermaßen lauten: 
+
+//"Meine Freunde sind Susi, Paula, Hans und Sepp" 
